e6b8c3652459df860b9d0d9c28f2a04f212f30c0
Murat Erdoğan <stormwr@gmail.com>
2020-02-07 21:10:02 +0300

    PaginationTagHelper iyileştirildi

---
+++ Data/Helpers/UrlExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Data.Models.Common;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+public static class UrlExtensions
+{
+    public static string GetPageLink(this HttpRequest httpRequest, int page, int? pageSize = null, string pageParamName = "page", string pageSizeParamName = "pagesize")
+    {
+        var queryParams = HttpUtility.ParseQueryString(httpRequest.QueryString.ToString());
+        if (queryParams[pageParamName] != null)
+        {
+            queryParams[pageParamName] = page.ToString();
+        }
+        else
+        {
+            queryParams.Add(pageParamName, page.ToString());
+        }
+
+        if(pageSize != null)
+        {
+            if (queryParams[pageSizeParamName] != null)
+            {
+                queryParams[pageSizeParamName] = pageSize.ToString();
+            }
+            else
+            {
+                queryParams.Add(pageSizeParamName, pageSize.ToString());
+            }
+        }
+
+        return $"{httpRequest.Path}?{queryParams.ToString()}";
+    }
+}
--- WebCMS/Areas/Admin/Shared/TagHelpers/PaginationTagHelper.cs
+++ WebCMS/Areas/Admin/Shared/TagHelpers/PaginationTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web;
 
 public class PaginationTagHelper : TagHelper
 {
@@ -23,6 +24,11 @@ public class PaginationTagHelper : TagHelper
         }
     }
 
+    private string buildUrlForPage(int page)
+    {
+        return httpRequest.GetPageLink(page);
+    }
+
 
     public PaginationTagHelper(IHttpContextAccessor contextAccessor)
     {
@@ -40,7 +46,7 @@ public class PaginationTagHelper : TagHelper
 
         var aHtml = new HtmlTag("a")
             .AddClass("page-link")
-            .Attr("href", "#")
+            .Attr("href", buildUrlForPage(pageNumber))
             .AppendText(pageNumber.ToString());
 
         liHtml.Append(aHtml);
@@ -54,7 +60,7 @@ public class PaginationTagHelper : TagHelper
 
         var aHtml = new HtmlTag("a")
             .AddClass("page-link")
-            .Attr("href", "#");
+            .Attr("href", buildUrlForPage(CurrentPage - 1));
 
         aHtml.Append(new HtmlTag("span").AddClass("mdi mdi-chevron-left"));
         aHtml.Append(new HtmlTag("span").AddClass("sr-only").AppendText("Previous"));
@@ -70,9 +76,41 @@ public class PaginationTagHelper : TagHelper
 
         var aHtml = new HtmlTag("a")
             .AddClass("page-link")
-            .Attr("href", "#");
+            .Attr("href", buildUrlForPage(CurrentPage + 1));
 
         aHtml.Append(new HtmlTag("span").AddClass("mdi mdi-chevron-right"));
+        aHtml.Append(new HtmlTag("span").AddClass("sr-only").AppendText("Previous"));
+
+        liHtml.Append(aHtml);
+
+        return liHtml;
+    }
+
+    private HtmlTag getFirstLink()
+    {
+        var liHtml = new HtmlTag("li").AddClass("page-item");
+
+        var aHtml = new HtmlTag("a")
+            .AddClass("page-link")
+            .Attr("href", buildUrlForPage(1));
+
+        aHtml.Append(new HtmlTag("span").AddClass("mdi mdi-chevron-double-left"));
+        aHtml.Append(new HtmlTag("span").AddClass("sr-only").AppendText("Previous"));
+
+        liHtml.Append(aHtml);
+
+        return liHtml;
+    }
+
+    private HtmlTag getLastLink()
+    {
+        var liHtml = new HtmlTag("li").AddClass("page-item");
+
+        var aHtml = new HtmlTag("a")
+            .AddClass("page-link")
+            .Attr("href", buildUrlForPage(TotalPageCount));
+
+        aHtml.Append(new HtmlTag("span").AddClass("mdi mdi-chevron-double-right"));
         aHtml.Append(new HtmlTag("span").AddClass("sr-only").AppendText("Previous"));
 
         liHtml.Append(aHtml);
@@ -94,19 +132,20 @@ public class PaginationTagHelper : TagHelper
             return;
         }
 
-        var navHtml = new HtmlTag("nav").Attr("aria-label", "Page navigation example");
+        var navHtml = new HtmlTag("nav").Attr("aria-label", "Page navigation example").AddClass("pagination");
 
         var ulHtml = new HtmlTag("ul").AddClass("pagination pagination-flat pagination-flat-rounded");
-
-        if (CurrentPage > 1)
-        {
-            ulHtml.Append(getPrevLink());
-        }
 
         var startPage = Convert.ToInt32((CurrentPage - Math.Floor((double)MaxPageCount / 2)));
         if (startPage <= 1)
         {
             startPage = 1;
+        }
+
+        if (CurrentPage > 1)
+        {
+            ulHtml.Append(getFirstLink());
+            ulHtml.Append(getPrevLink());
         }
 
         var endPage = (startPage + MaxPageCount - 1);
@@ -133,6 +172,7 @@ public class PaginationTagHelper : TagHelper
         if (CurrentPage < TotalPageCount)
         {
             ulHtml.Append(getNextLink());
+            ulHtml.Append(getLastLink());
         }
 
         navHtml.Append(ulHtml);
