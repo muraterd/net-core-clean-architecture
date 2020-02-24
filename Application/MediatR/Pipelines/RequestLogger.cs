be2b60461b3e25584ad14528f248e6ec6bdde03b
Murat Erdoğan <stormwr@gmail.com>
2020-02-24 18:46:52 +0300

    Mediatr RequestLogger içerisine email ve id eklendi

--- Application/MediatR/Pipelines/RequestLogger.cs
+++ Application/MediatR/Pipelines/RequestLogger.cs
@@ -1,32 +1,24 @@
-﻿using FluentValidation;
-using MediatR;
+﻿using Data;
 using MediatR.Pipeline;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
 {
     private readonly ILogger logger;
-    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly CurrentUser currentUser;
 
-    public RequestLogger(ILogger<TRequest> logger, IHttpContextAccessor _httpContextAccessor)
+    public RequestLogger(ILogger<TRequest> logger, CurrentUser currentUser)
     {
         this.logger = logger;
+        this.currentUser = currentUser;
     }
 
     public async Task Process(TRequest request, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
-        //var userId
-        //var username
 
-        logger.LogInformation("Request {Name} {@Request}", requestName, request);
-        //logger.LogInformation("Request {Name} {@userId} {@userName} {@Request}", requestName, userId);
+        logger.LogInformation("Request {Name} {@userId} {@email} {@Request}", requestName, currentUser.Id, currentUser.Email, request);
     }
 }
